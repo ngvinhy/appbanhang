5b4d66e5df3c4bf8bcb8315090ff132153427b11
ngvinhy <ngvinhy@users.noreply.github.com>
2023-04-29 19:55:33 +0700

    ngvinhy

--- main.py
+++ main.py
@@ -19,18 +19,18 @@ def register():
     register_screen.resizable(width=False, height=False)
     username = StringVar()
     password = StringVar()
-    Label(register_screen, text="Please enter details below", bg="pink").pack()
+    Label(register_screen, text="Please enter details below").pack()
     Label(register_screen, text="").pack()
-    username_lable = Label(register_screen, text="Username * ")
+    username_lable = Label(register_screen, text="Username")
     username_lable.pack()
     username_entry = Entry(register_screen, textvariable=username)
     username_entry.pack()
-    password_lable = Label(register_screen, text="Password * ")
+    password_lable = Label(register_screen, text="Password")
     password_lable.pack()
     password_entry = Entry(register_screen, textvariable=password, show='*')
     password_entry.pack()
     Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=10, height=1, bg="pink", command=register_user).pack()
+    Button(register_screen, text="Register", width=10, height=1, command=register_user).pack()
 
 
 def login():
@@ -49,11 +49,11 @@ def login():
 
     global username_login_entry
     global password_login_entry
-    Label(login_screen, text="Username * ").pack()
+    Label(login_screen, text="Username").pack()
     username_login_entry = Entry(login_screen, textvariable=username_verify)
     username_login_entry.pack()
     Label(login_screen, text="").pack()
-    Label(login_screen, text="Password * ").pack()
+    Label(login_screen, text="Password").pack()
     password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
     password_login_entry.pack()
     Label(login_screen, text="").pack()
@@ -68,11 +68,8 @@ def register_user():
     file.write(username_info + "\n")
     file.write(password_info)
     file.close()
-
-    username_entry.delete(0, END)
-    password_entry.delete(0, END)
-
-    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+    messagebox.showinfo("Account", "Registration Success")
+    register_screen.destroy()
 
 
 def login_verify():
@@ -136,8 +133,8 @@ def main_account_screen():
     img1 = tk.Label(login_frame, image=image_cover)
     img1.pack(side="top", fill=BOTH)
 
-    Button(login_frame, text="Login", height="2", width="30", command=login).place(x=320, y=250)
-    Button(login_frame, text="Register", height="2", width="30", command=register).place(x=320, y=300)
+    Button(login_frame, text="Login", height="2", width="15", command=login, font=("Comic Sans MS", 10, BOLD)).place(x=360, y=260)
+    Button(login_frame, text="Register", height="2", width="15", command=register, font=("Comic Sans MS", 10, BOLD)).place(x=360, y=320)
 
     main_screen.mainloop()
 
